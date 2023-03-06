ffb75929b06fb7d346a00f225d2ef1920498d416
Federico <federico.capaldo@hotmail.com>
2023-03-06 02:13:49 +0000

    connecting to correct dynamic chatroom instead of harcoded chatroom

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,8 +6,13 @@ export default class extends Controller {
   static targets = [
     "messages",
   ]
+  static values = {
+    chatroomId: Number
+  }
 
   connect() {
-    createConsumer().subscriptions.create("ChatroomChannel")
+    createConsumer().subscriptions.create({
+      channel: "ChatroomChannel", id: this.chatroomIdValue
+    });
   }
 }
