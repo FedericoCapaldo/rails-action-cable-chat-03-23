0e916dd2963bdda4d4ebdf3c7e115170594f7467
Federico <federico.capaldo@hotmail.com>
2023-03-06 02:04:35 +0000

    crated simplest connection from front-end to actioncable channel

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+// Connects to data-controller="chatroom-subscription"
+export default class extends Controller {
+  static targets = [
+    "messages",
+  ]
+
+  connect() {
+    createConsumer().subscriptions.create("ChatroomChannel")
+  }
+}
